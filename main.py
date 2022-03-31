3b3de10d00e2131e98c5961cd49ea9bc77927f66
Hadrien <chicault.hadrien@gmail.com>
2022-03-31 18:07:57 +0200

    debut du programme

---
+++ main.py
@@ -0,0 +1,11 @@
+from utils import DataClassCard
+
+
+def main():
+    queen_of_hearts = DataClassCard('Q', 'Hearts')
+
+    print(queen_of_hearts.rank)
+
+
+if __name__ == "__main__":
+    main()
---
+++ utils.py
@@ -0,0 +1,6 @@
+from dataclasses import dataclass
+
+@dataclass
+class DataClassCard:
+    rank: str
+    suit: str
