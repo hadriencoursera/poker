293f28d93118f5d4af230cb074bb5e6bb416db17
Hadrien <chicault.hadrien@gmail.com>
2022-03-31 18:15:46 +0200

    svg

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from utils import DataClassCard
 
 def main():
     queen_of_hearts = DataClassCard('Q', 'Hearts')
-
     print(queen_of_hearts.rank)
+    print(queen_of_hearts)
 
 
 if __name__ == "__main__":
